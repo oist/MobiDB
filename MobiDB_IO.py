dee07e39b83e25e464ff395aa0847a8e3d169762
takaya-shimabukuro <faka0000empty@gmail.com>
2019-08-31 17:40:14 +0900

    MobiDB_IOとThreadの機能を分割

--- MobiDB_IO.py
+++ MobiDB_IO.py
@@ -1,17 +1,4 @@
 from bioservices import UniProt  # Uniprotのメソッドをインポート
-import threading
-import linecache
-from time import sleep
-
-
-def worker(leng, i):
-    for s in (0, leng):
-        query = linecache.getline(fr, int(s + leng * i))  # (現在の行) + (実行した行)
-        result = service.search("id:" + query)
-        fw.write(result + "\n")
-        linecache.clearcache()
-    return
-
 if __name__ == '__main__':
     service = UniProt()
 
@@ -19,28 +6,16 @@ if __name__ == '__main__':
         with open("MobiDB_ID_small.txt") as fr:
 
             fr_line = fr.readlines()
-            leng = int(len(fr_line))
-            threads = []
-
-        for i in range(10):
-
-            t = threading.Thread(target=worker, args=(leng,i))
-            threads.append(t)
-            t.start()
-            sleep(1)
+            leng = len(fr_line)
 
 
-        for thread in threads:
-            thread.join()
+        for line in fr_line:
+
+            query = line
+            result = service.search("id:" + query)
+            fw.write(result + "\n")
 
 
     print("finish")
 
-
-
-#         for offset, item in enumerate(fr_line):
-#            query = item
-#            t = threading.Thread(target=worker, args=(offset,))
-#            threads.append(t)
-#            t.start()
 
