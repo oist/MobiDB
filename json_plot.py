769505aedc7cf9d7554544a21ffbd63b7a6eb529
ucpr <hoge.uchihara@gmail.com>
2019-11-05 17:01:56 +0900

    再描画する部分を減らして高速化を行った
    - bugが残っている
    　　- 画面を最大化したらstack overflowになる

--- json_plot.py
+++ json_plot.py
@@ -1,10 +1,10 @@
 from logging import getLogger, StreamHandler, DEBUG
 import json
+import time
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 
-"""デバック"""
+
 logger = getLogger(__name__)
 handler = StreamHandler()
 handler.setLevel(DEBUG)
@@ -14,8 +14,11 @@ logger.propagate = False
 
 
 class ScorePlot:
-    def __init__(self, **kwargs):
-        logger.debug("ScorePlot_ini Begin")
+    def __init__(self):
+        self.fig, self.ax = plt.subplots()
+        self.ln_v = self.ax.axvline(0)
+        self.ln_h = self.ax.axhline(0)
+        self.fig.canvas.mpl_connect("motion_notify_event", self.on_motion)
 
         self.json_dict = {}
         self.list_id = []
@@ -25,26 +28,28 @@ class ScorePlot:
         self.pName = ""
         self.div = 0
         self.threshold = 0.75
-        self.ln_v = plt.axvline(0)
         self.text = ""
 
-        logger.debug("ScorePlot_init End")
+        self.load_propaty()
+        self.plot_json_data()
+        self.fig.canvas.draw()
+
+        ## canvasの一部を再描画するためのやつ
+        self.bg = self.fig.canvas.copy_from_bbox(self.ax.bbox)
+
 
     def load_propaty(self):
-        logger.debug("load_propaty Begin")
+        logger.debug('load_propaty Begin')
 
         div = 0
 
-        with open("disorder_add_protain.mjson", "r") as f:
+        with open("disorder_add_protain.mjson", 'r') as f:
             for (k, line) in enumerate(f):
                 if k == 20020:
                     self.json_dict = json.loads(line)
-
                     break
 
-        self.score = self.json_dict["mobidb_consensus"]["disorder"]["predictors"][1][
-            "scores"
-        ]  # scoreの値を取得する
+        self.score = self.json_dict["mobidb_consensus"]["disorder"]["predictors"][1]["scores"]  # scoreの値を取得する
         self.sequence = list(self.json_dict["sequence"])  # シーケンスの値を取得する
         self.acc = self.json_dict["acc"]  # Entry nameを取得する
         try:
@@ -57,75 +62,52 @@ class ScorePlot:
 
             self.list_id.append(i)
 
-        self.text = (
-            "ACC:"
-            + self.acc
-            + "\n"
-            + "Protain Names : "
-            + self.pName
-            + "\n"
-            + "Percentage (High):"
-            + str(round(div / len(self.score) * 100, 3))
-            + "%"
-        )
+        self.text = "ACC:" + self.acc + "\n" + \
+                    "Protain Names : " + self.pName + "\n" + \
+                    "Percentage (High):" + str(round(div / len(self.score) * 100, 3)) + "%"
 
-        logger.debug("load_propaty End")
-
-    def set_plt(self):
-        logger.debug("set_plot Begin")
-
-        # plt.plots(sp.core, color='black', alpha=0.5)self.sequence
-        plt.scatter(self.list_id, self.score, s=25, c=self.score, cmap="jet")
-        plt.plot(self.score, color="black", linestyle="solid", alpha=0.7)
+    def plot_json_data(self):
+        plt.scatter(self.list_id, self.score, s=25, c=self.score, cmap='jet')
+        plt.plot(self.score, color='black', linestyle='solid', alpha=0.7)
 
         plt.ylim(0, 1.1)
-        plt.xlabel("Array", fontsize=16)
-        plt.ylabel("Score", fontsize=16)
+        plt.xlabel('Array', fontsize=16)
+        plt.ylabel('Score', fontsize=16)
         plt.colorbar()
 
         plt.grid(True)
 
-        ax = plt.gca()
-        ax.spines["right"].set_color("none")  # 右枠消し
-        ax.spines["top"].set_color("none")  # 上枠消し
-        ax.spines["left"].set_color("m")  # 左枠をマゼンダに
-        ax.spines["bottom"].set_color("c")
-        ax.text(
-            0,
-            1.1,
-            self.text,
-            fontweight="semibold",
-            style="italic",
-            bbox={"facecolor": "blue", "alpha": 0.3, "pad": 10},
-        )
+        self.ax.spines["right"].set_color("none")  # 右枠消し
+        self.ax.spines["top"].set_color("none")  # 上枠消し
+        self.ax.spines["left"].set_color("m")  # 左枠をマゼンダに
+        self.ax.spines["bottom"].set_color("c")
+        self.ax.text(0, 1.1, self.text, fontweight="semibold", style='italic',
+                bbox={'facecolor': 'blue', 'alpha': 0.3, 'pad': 10})
 
         for i in range(len(self.score)):
-            ax.annotate(
-                self.sequence[i], (i, 1.05), size=8, horizontalalignment="center"
-            )
+            self.ax.annotate(self.sequence[i], (i, 1.05), size=5, horizontalalignment='center')
 
         plt.hlines([self.threshold], 0, len(self.score), "r", linestyle=":", lw=1)
 
         plt.tight_layout()
-        logger.debug("set_plot End")
+        self.fig.canvas.draw()
+
+    def on_motion(self, event):
+        self.ln_v.set_xdata(event.xdata)
+        self.ln_h.set_ydata(event.ydata)
+
+        self.fig.canvas.restore_region(self.bg)
+        self.ax.draw_artist(self.ln_h)
+        self.ax.draw_artist(self.ln_v)
+        self.fig.canvas.blit(self.ax.bbox)
+        self.fig.canvas.flush_events()
 
 
-def motion(event):
-    x = event.xdata
-    sp.ln_v.set_xdata(x)
-    plt.draw()
+    def run(self):
+        self.fig.show()
 
 
 if __name__ == "__main__":
-    logger.debug("main Begin")
-
-    sp = ScorePlot()
-
-    sp.load_propaty()
-    sp.set_plt()
-
-    plt.connect("motion_notify_event", motion)
-
+    score = ScorePlot()
+    score.run()
     plt.show()
-
-    logger.debug("main End")
