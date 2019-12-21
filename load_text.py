7a54662b77e1022187dc3c4e44ee880bcb7e4b34
takaya-shimabukuro <faka0000empty@gmail.com>
2019-12-21 12:53:08 +0900

    プロセス終了時、toastで表示する機能を追加

---
+++ load_text.py
@@ -0,0 +1,54 @@
+import config
+from logging import getLogger, StreamHandler, DEBUG
+
+"""デバック"""
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+
+class Load:
+    """
+    app.sm.current == "search" 時の処理
+
+    Methods
+    ----------
+
+    substitute_text(self, str = num)
+        textから取得したデータは必ずstr型である。
+        str型　→　float型に変換するときは、""を取り除く必要がある。
+
+    Raises
+    ------
+    ValueError
+    入力された文字が数字以外のときに発生。
+
+    Notes
+    ----------
+    Button イベントは theme.ScreenSearchに記載。
+    config.threshold_val, config.threshold_len, config.fill_gapの使い方は
+    screen_wait_search.pyに記載。
+
+    """
+
+    def load_text(self, th_val, th_len, fill_gap):
+        logger.debug("load_text.py, Load, load_text()")
+
+        try:
+            config.threshold_val = self.substitute_text(th_val)
+            config.threshold_len = int(self.substitute_text(th_len))
+            config.fill_gap = int(self.substitute_text(fill_gap))
+
+        except ValueError as e:
+            print(e)
+
+    def substitute_text(self, ss_text):
+        logger.debug("load_text.py, Load, substitute_text()")
+
+        if ss_text == "":
+            return 0
+        else:
+            return float(ss_text.replace('"', ''))
--- main.py
+++ main.py
@@ -1,38 +1,53 @@
 import sys
-from search_data import SearchData
+from logging import getLogger, StreamHandler, DEBUG
+
 from kivy.lang import Builder
-from kivy.factory import Factory
-from kivymd.app import MDApp
 from kivy.core.window import Window
+
+from kivymd.app import MDApp
+from kivymd.toast.kivytoast.kivytoast import toast
+
+from load_text import Load
 from search_data import SearchData
-import config
+
+
+"""デバック"""
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
 
 
 class MainApp(MDApp):
     def __init__(self, **kwargs):
+        logger.debug("main.py, MainApp, __init__()")
         self.title = "MobiDB -Human"
         self.theme_cls.primary_palette = "Blue"
         self.sd = SearchData()
+        self.load = Load()
         super().__init__(**kwargs)
 
     def build(self):
+        logger.debug("main.py, MainApp, build()")
         with open("./theme.kv", "r", encoding="utf8") as f:
             self.root = Builder.load_string(f.read())
 
     def exit(self):
+        logger.debug("main.py, MainApp, exit()")
         sys.exit(1)
 
     def btn_event(self):
-        print("------------------------")
-        print(config.threshold_val)
-        print(config.threshold_len)
-        print(config.fill_gap)
-        print("------------------------")
-        self.sd.get_text(self.root.ids["th_val"].text, self.root.ids["th_len"].text, self.root.ids["fill_gap"].text)
-        print(config.threshold_val)
-        print(config.threshold_len)
-        print(config.fill_gap)
-        print("------------------------")
+        logger.debug("main.py, MainApp, btn_event()")
+        self.load.load_text(self.root.ids["th_val"].text, self.root.ids["th_len"].text, self.root.ids["fill_gap"].text)
+
+        self.sd = SearchData()
+        self.sd.start()
+
+    def show_toast(self):
+        logger.debug("main.py, MainApp, show_toast()")
+        toast("Load is Successed")
 
 
 if __name__ == "__main__":
