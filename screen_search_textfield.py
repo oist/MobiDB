e68d7ad506bc19b1e3840346ffa30987ae105642
takaya-shimabukuro <faka0000empty@gmail.com>
2019-12-30 15:59:27 +0900

    checkboxのactiveを取得できるようにした

---
+++ screen_search_textfield.py
@@ -0,0 +1,10 @@
+from logging import getLogger, StreamHandler, DEBUG
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen
+from kivymd.uix.tab import MDTabsBase
+
+import config
+from kivy.app import App
+
+class StyleFilter(Screen):
+    pass
