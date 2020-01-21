f9292aa21707ead9d6ade6c024f8bf429c5b3999
ucpr <hoge.uchihara@gmail.com>
2020-01-21 18:30:58 +0900

    faet: 検索画面でEnterでsubmitできるようにした

--- screen_search_keyword_main.py
+++ screen_search_keyword_main.py
@@ -1,7 +1,9 @@
 from logging import getLogger, StreamHandler, DEBUG
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.tab import MDTabsBase
+from kivy.properties import ObjectProperty
 
 import config
 from kivy.app import App
@@ -17,6 +19,13 @@ logger.propagate = False
 
 
 class ScreenSearchKeyword(Screen, BoxLayout, MDTabsBase):
+    submit = ObjectProperty(None)
+    def __init__(self, **kwargs):
+        super(ScreenSearchKeyword, self).__init__(**kwargs)
+
+        Window.bind(on_key_down=self._on_keyboard_down)
+
+
     def btn_event(self):
         logger.debug("screen_search_main.py, ScreenSearch, btn_event()")
 
@@ -39,3 +48,7 @@ class ScreenSearchKeyword(Screen, BoxLayout, MDTabsBase):
 
         app = App.get_running_app()
         app.sm.current = name
+    def _on_keyboard_down(self, instance, keyboard, keycode, text, modifiers):
+        app = App.get_running_app()
+        if app.sm.current == "search_keyword" and keycode == 40:  # 40 - Enter key pressed
+            self.btn_event()
--- screen_search_main.py
+++ screen_search_main.py
@@ -1,7 +1,9 @@
 import time
 from logging import getLogger, StreamHandler, DEBUG
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen
+from kivy.properties import ObjectProperty
 from kivymd.uix.tab import MDTabsBase
 
 import config
@@ -43,6 +45,11 @@ class ScreenSearchFilter(Screen, BoxLayout, MDTabsBase):
     screen_wait_search.pyに記載。
 
     """
+    submit = ObjectProperty(None)
+    def __init__(self, **kwargs):
+        super(ScreenSearchFilter, self).__init__(**kwargs)
+
+        Window.bind(on_key_down=self._on_keyboard_down)
 
     def btn_event(self):
         logger.debug("screen_search_main.py, ScreenSearch, btn_event()")
@@ -79,3 +86,8 @@ class ScreenSearchFilter(Screen, BoxLayout, MDTabsBase):
 
         app = App.get_running_app()
         app.sm.current = name
+
+    def _on_keyboard_down(self, instance, keyboard, keycode, text, modifiers):
+        app = App.get_running_app()
+        if app.sm.current == "search_filter" and keycode == 40:  # 40 - Enter key pressed
+            self.btn_event()
